a6b0995cd9422e46091cb29d7126cc560b3a2da8
sagor.askar <sagor.askar@gmail.com>
2023-08-06 23:59:26 +0600

    Product & Role Controller updated

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+
+    function __contruct()
+    {
+        $this->middleware('permission:product-list|product-create|product-edit|priduct-delete', ['only' => ['index', 'show']]);
+        $this->middleware('permission:product-create', ['only' => ['create','store']]);
+        $this->middleware('permission:product-edit', ['only' => ['edit','update']]);
+        $this->middleware('permission:product-delete', ['only' => ['destroy']]);
+    }
+
+    public function index()
+    {
+        // function for paginate and proper data display
+        $products = Product::latest()->paginate(5);
+        return view('products.index', compact('products'))->with('i', (request()->input('page', 1) - 1) * 5);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('products.edit');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        request()->validate([
+            'name' => 'required',
+            'detail' => 'required',
+        ]);
+        Product::create($request->all());
+        return redirect()->route('products.index')->with('message', 'Products Created Successfully!');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Product $product)
+    {
+        return view('products.show', compact('product'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Product $product)
+    {
+        return view('product.edit', compact('product'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Product $product)
+    {
+        request()->validate([
+            'name' => 'required',
+            'detail' => 'required',
+        ]);
+        $product->update($request->all());
+        return redirect()->route('products.index')->with('message', 'Product Updated Successfully!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Product $product)
+    {
+        $product->delete();
+        return redirect()->route('products.index')->with('message', 'Product Deleted Successfully!');
+    }
+}
